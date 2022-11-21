56b0361a94bd00699e8eb4b722f2496728d94844
Martin Lingeri <63207626+MartinLingeri@users.noreply.github.com>
2022-11-21 02:05:14 -0300

    edit drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Box, Stack } from '@chakra-ui/react'
+import { useRef, useState } from 'react'
+import { Box, Stack, useDisclosure } from '@chakra-ui/react'
 
 import './App.css'
 
@@ -10,6 +10,7 @@ import Scrollbar from './components/Scrollbar'
 import GameBanner from './components/GameBanner'
 import GameInfo from './components/GameInfo'
 import GameFeedbackButtons from './components/GameFeedbackButtons'
+import Edit from './components/Edit'
 
 import GameBackgroundShadow from './assets/game_page_background_shadow.png'
 import PageContent from './components/PageContent'
@@ -17,51 +18,58 @@ import PageContent from './components/PageContent'
 function App() {
   const [bgImage, setBgImage] = useState('https://picsum.photos/1438/810')
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = useRef(null)
+
   return (
-    <Box overflow='hidden'>
-      <Navbar />
-      <Stack
-        background='primary.600'
-        height='100%'
-        minH='100vh'
-        alignItems='center'
-        position='relative'
-        boxShadow='0 0 100px 50px #1A2938 inset'
-        _before={{
-          content: '""',
-          position: 'absolute',
-          width: '100%',
-          height: '800px',
-          backgroundImage: `url(${bgImage})`,
-          backgroundPosition: 'center top',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          filter:
-            'sepia(100%) hue-rotate(190deg) saturate(400%) brightness(20%) opacity(20%)',
-          boxShadow: 'inset 0px -60px 50px 100px #78bbff',
-          zIndex: 0,
-        }}
-        _after={{
-          content: '""',
-          position: 'absolute',
-          top: '564px;',
-          width: '100%',
-          height: '100%',
-          backgroundImage: `url(${GameBackgroundShadow})`,
-          backgroundRepeat: 'no-repeat',
-        }}
-      >
-        <Stack width='940px' gap='16px'>
-          <StoreNav />
-          <Title />
-          <GameBanner />
-          <GameInfo />
-          <GameFeedbackButtons />
-          <PageContent />
+    <>
+      <Edit isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
+      <Box overflow='hidden'>
+        <Navbar onOpen={onOpen} btnRef={btnRef} />
+        <Stack
+          background='primary.600'
+          height='100%'
+          minH='100vh'
+          alignItems='center'
+          position='relative'
+          boxShadow='0 0 100px 50px #1A2938 inset'
+          _before={{
+            content: '""',
+            position: 'absolute',
+            width: '100%',
+            height: '800px',
+            backgroundImage: `url(${bgImage})`,
+            backgroundPosition: 'center top',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            filter:
+              'sepia(100%) hue-rotate(190deg) saturate(400%) brightness(20%) opacity(30%)',
+            boxShadow: 'inset 0px -60px 50px 100px #78bbff',
+            zIndex: 0,
+          }}
+          _after={{
+            content: '""',
+            position: 'absolute',
+            top: '564px;',
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(${GameBackgroundShadow})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'top',
+          }}
+        >
+          <Stack width='940px' gap='16px'>
+            <StoreNav />
+            <Title />
+            <GameBanner />
+            <GameInfo />
+            <GameFeedbackButtons />
+            <PageContent />
+          </Stack>
         </Stack>
-      </Stack>
-      <Scrollbar />
-    </Box>
+        <Scrollbar />
+      </Box>
+    </>
   )
 }
 
--- src/components/GameFeature.tsx
+++ src/components/GameFeature.tsx
@@ -1,38 +1,24 @@
 import { Box } from '@chakra-ui/react'
 
-import IcoAchievements from '../assets/gameFeaturesIcons/ico_achievements.png'
-import IcoCart from '../assets/gameFeaturesIcons/ico_cart.png'
-import IcoCards from '../assets/gameFeaturesIcons/ico_cards.png'
-import IcoCloud from '../assets/gameFeaturesIcons/ico_cloud.png'
-import IcoController from '../assets/gameFeaturesIcons/ico_controller.png'
-import IcoCoop from '../assets/gameFeaturesIcons/ico_coop.png'
-import IcoRemotePlay from '../assets/gameFeaturesIcons/ico_remote_play.png'
-import IcoSinglePlayer from '../assets/gameFeaturesIcons/ico_singlePlayer.png'
-import IcoMultiPlayer from '../assets/gameFeaturesIcons/ico_multiPlayer.png'
+import { gameFeatures } from './gameFeatures'
 
 export default function GameFeature({ feature }: { feature: string }) {
-  const gameFeatures = {
-    achievements: { icon: IcoAchievements, alt: 'Logros de Steam' },
-    cart: { icon: IcoCart, alt: 'Compras dentro de la aplicacion' },
-    cards: { icon: IcoCards, alt: 'Cromos de Steam' },
-    cloud: { icon: IcoCloud, alt: 'Steam Cloud' },
-    controller: { icon: IcoController, alt: 'Compat. total con mando' },
-    coopOnline: { icon: IcoCoop, alt: 'Cooperativo en Linea' },
-    coopLan: { icon: IcoCoop, alt: 'Cooperativo en LAN' },
-    coopScreenSplit: { icon: IcoCoop, alt: 'Coop a pantalla partida' },
-    remotePlay: { icon: IcoRemotePlay, alt: 'Jugar en cualquier lugar' },
-    singlePlayer: { icon: IcoSinglePlayer, alt: 'Juego para un jugador' },
-    multiPlayer: { icon: IcoMultiPlayer, alt: 'Juego para varios jugadores' },
-  }
-
   return (
-    <Box display='flex' flexDirection='row' width='276px' height='26px'>
+    <Box
+      role='group'
+      display='flex'
+      flexDirection='row'
+      width='276px'
+      height='26px'
+      cursor='pointer'
+      zIndex='100'
+    >
       <Box
         display='flex'
         alignItems='center'
         justifyContent='center'
         backgroundColor='#00000033'
-        width="38px"
+        width='38px'
       >
         <img
           width='26px'
@@ -43,12 +29,13 @@ export default function GameFeature({ feature }: { feature: string }) {
       <Box
         display='flex'
         alignItems='center'
-        color="primary.100"
-        fontSize="12px"
+        color='primary.100'
+        fontSize='12px'
         width='100%'
         height='100%'
         padding='6px 8px'
         backgroundColor='rgba( 103, 193, 245, 0.1 )'
+        _groupHover={{ backgroundColor: 'primary.50', color: 'white' }}
       >
         {gameFeatures[feature as keyof typeof gameFeatures].alt}
       </Box>
